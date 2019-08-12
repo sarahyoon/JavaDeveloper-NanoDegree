030a819ee72e634033d3098404dca0117baf0126
unknown <yoonsarah88@gmail.com>
2019-08-12 18:19:26 +0900

    1. create send and show messages 2. disconnect chat

--- chatroom/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ chatroom/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -1,9 +1,13 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.rmi.server.ExportException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,8 +27,12 @@ public class WebSocketChatServer {
      */
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
-    private static void sendMessageToAll(String msg) {
-        //TODO: add send message method.
+    private static void sendMessageToAll(String msg, Session session) {
+        try{
+            onlineSessions.get(session.getId()).getBasicRemote().sendText(msg);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -32,8 +40,7 @@ public class WebSocketChatServer {
      */
     @OnOpen
     public void onOpen(Session session) {
-        //TODO: add on open connection.
-        onlineSessions.put("session", session);
+        onlineSessions.put(session.getId(), session);
     }
 
     /**
@@ -41,15 +48,19 @@ public class WebSocketChatServer {
      */
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
-        //TODO: add send message.
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        jsonObject.put("type", "SPEAK");
+        jsonStr = jsonObject.toJSONString();
+
+        sendMessageToAll(jsonStr, session);
     }
 
     /**
      * Close connection, 1) remove session, 2) update user.
      */
     @OnClose
-    public void onClose(Session session) {
-        //TODO: add close connection.
+    public void onClose(Session session) throws Exception {
+        onlineSessions.get(session.getId()).close();
     }
 
     /**
