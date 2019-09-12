c9207488b54e401c1a280f0effae7217294b283e
sarahyoon <yoonsarah88@gmail.com>
2019-09-12 12:00:48 +0900

    create dog microservice client

---
+++ lab/microservices/dogMicroservice/src/main/java/com/udacity/dogMicroservice/DogMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.dogMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogMicroserviceApplication.class, args);
+	}
+
+}
