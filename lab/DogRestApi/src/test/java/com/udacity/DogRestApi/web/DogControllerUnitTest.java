69d100627c09c6deca3f482b1e626849996f1288
sarahyoon <yoonsarah88@gmail.com>
2019-09-12 23:12:22 +0900

    create dogController unit test

---
+++ lab/DogRestApi/src/test/java/com/udacity/DogRestApi/web/DogControllerUnitTest.java
@@ -0,0 +1,46 @@
+package com.udacity.DogRestApi.web;
+
+import com.udacity.DogRestApi.entity.Dog;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class DogControllerUnitTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAllDogs(){
+        ResponseEntity<List> response = this.restTemplate.getForEntity("http://localhost:" + port + "/dogs/", List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void getBreedByID(){
+        ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/1/breed/", String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+
+}
