decab83e3090e355305abe02858b1c14a3a3e13a
sarahyoon <yoonsarah88@gmail.com>
2019-09-13 22:47:30 +0900

    add vehiclesAPI project

---
+++ projects/vehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -0,0 +1,19 @@
+package com.udacity.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Creates a Spring Boot Application to run the Pricing Service.
+ * TODO: Convert the application from a REST API to a microservice.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
+
+}
