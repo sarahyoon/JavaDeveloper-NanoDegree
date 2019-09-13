55ab8df975c0d8f82e50e0fe7c23013049d92b8b
sarahyoon <yoonsarah88@gmail.com>
2019-09-13 23:45:57 +0900

    add READ, CREATE operations from vehiclesAPI

--- projects/vehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ projects/vehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -80,7 +80,8 @@ class CarController {
          * TODO: Use the `assembler` on that saved car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+        car = carService.save(car);
+        Resource<Car> resource = assembler.toResource(car);
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
