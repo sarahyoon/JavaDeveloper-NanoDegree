696e226a2538b63cf839bf4791657a2ef1037756
sarahyoon <yoonsarah88@gmail.com>
2019-10-13 23:11:58 +0900

    [Add] password check length and password confirmation

--- e-commerce App/src/main/java/com/example/demo/controllers/UserController.java
+++ e-commerce App/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,9 @@ public class UserController {
 	@Autowired
 	private CartRepository cartRepository;
 
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
 	@GetMapping("/id/{id}")
 	public ResponseEntity<EcommerceUser> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
@@ -40,6 +44,13 @@ public class UserController {
 	public ResponseEntity<EcommerceUser> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		EcommerceUser user = new EcommerceUser();
 		user.setUsername(createUserRequest.getUsername());
+
+		if (createUserRequest.getPassword().length() < 7 || createUserRequest.getPassword() == null
+				|| !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			return ResponseEntity.badRequest().build();
+		}
+		user.setPassword(bcryptEncoder.encode(createUserRequest.getPassword()));
+
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
